550852b88b9cedf0047bdc4d2da9204c5e5d2564
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2023-12-18 18:22:10 +0200

    clear logs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { store } from 'store/store';
 import './index.css';
 import App from 'components/App';
 import { Provider } from 'react-redux';
-console.log(store.getState())
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
